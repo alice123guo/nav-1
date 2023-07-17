0b6fc9825705ae9db956872b8606b44457fb3c58
Alice <guoxinyaya@163.com>
2023-07-17 15:28:06 +0800

    6.弹出网页让用户输入网址:prompt，如果开头不是https开头的:if(url.indexOf("http")!===0){ ，我们给用户加上:url = 'https://'+url

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('1')
+$('.addButton')
+    .on('click',()=>{
+        console.log(1)
+        let url = window.prompt("请问你要添加的网址是：")
+        if(url.indexOf("http")!==0){
+            url = "https://"+url
+            // alert("请输入以http开头的网址")
+            console.log(url)
+        }4
+    })
