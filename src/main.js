04dc17c762add3145fbef38a38721aee562b51cc
Alice <guoxinyaya@163.com>
2023-07-19 12:47:52 +0800

    8.新建一个hashMap，然后遍历（hashMap.forEach）这个表格，就可以在JS中把哈希表格的结构写出来，然后可以删HTML里的LI，但是注意不可以删lastLi

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
+//因为li是在siteList里面的，所以需要把siteList放前面
+const $siteList = $(".siteList")
+const $lastLi = $siteList.find('li.last')
+
+
+const hashMap = [
+  {logo:'A',logoType:'text',url:'https://www.acfun.cn'},
+  {logo:'./images/bilibili.svg',logoType:'image',url:'https://www.bilibili.com'},
+  {logo:'./images/百度.svg',logoType:'image',url:'https://www.baidu.com'}
+]
+
+hashMap.forEach(node=>{
+  const $li = $(`
+        <li>
+          <a href="${node.url}">
+            <div class="site">
+              <div class="logo">${node.logo[0]}</div>
+              <div class="link">${node.url}</div>
+            </div>
+          </a>
+        </li>
+  `).insertBefore($lastLi)//
+})
+
 $('.addButton')
     .on('click',()=>{
         console.log(1)
@@ -7,9 +31,9 @@ $('.addButton')
             // alert("请输入以http开头的网址")
             console.log(url)
         }
-        const $siteList = $(".siteList")//先找到siteList
-        console.log($siteList)///jQuery返回的是一个API 
-        const $lastLi = $siteList.find('li.last')
+        // const $siteList = $(".siteList")//先找到siteList
+        // console.log($siteList)///jQuery返回的是一个API 
+        // const $lastLi = $siteList.find('li.last')
         const $li = $(` <li>
           <a href="${url}">
             <div class="site">
